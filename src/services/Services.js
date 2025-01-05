7cf22617a4b2c714e980f76f5c86cab4f74f16eb
Yash <yash.fkkfr22@sinhgad.edu>
2025-01-05 20:56:46 +0530

    Service file changes

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,5 +1,5 @@
-import HttpInterceptor from "../components/ServiCeProvider/HttpInterceptor";
-import { AUTH } from "./APIURLs";
+import HttpInterceptor from "./HttpInterceptor";
+import { AUTH, FILES } from "./APIURLs";
 
 const apiServiceBased = HttpInterceptor();
 
@@ -12,5 +12,32 @@ export default {
   },
   getProfile: async () => {
     return apiServiceBased.get(`${AUTH}/profile`);
+  },
+  getPreSignedUrlToUpload: async (fileData) => {
+    return apiServiceBased.post(`${FILES}/presinged-url`, {
+      filename: fileData.filename,
+      contentType: fileData.contentType,
+      userId: fileData.userId,
+    });
+  },
+  putFileMetaDataToMongoDb: async (fileData) => {
+    return apiServiceBased.post(`${FILES}/save-to-mongo`, {
+      filename: fileData.filename,
+      contentType: fileData.contentType,
+      userId: fileData.userId,
+      key: fileData.key,
+    });
+  },
+  getPreSignedUrlToAccess: async (userId, key) => {
+    try {
+      const response = await apiServiceBased.post(`${FILES}/access`, {
+        userId,
+        key,
+      });
+
+      return response;
+    } catch (error) {
+      return error;
+    }
   },
 };
